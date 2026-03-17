4bea554c952b1a2d90eeee116d7a6ed0ea81351b
Oliver <olitaylor52@outlook.com>
2026-03-17 18:53:07 +0000

    Add PyPI stuffs

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='disneyqueuetimes',
+    version='2026.1',
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+)
