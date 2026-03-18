5155c242c84d31dca8625d17b90f0e6c5481cf59
Oliver <olitaylor52@outlook.com>
2026-03-18 18:30:39 +0000

    Change API from Queue-Times.com to ThemeParks.wiki, updated version number

--- disneyqueuetimes/main.py
+++ disneyqueuetimes/main.py
@@ -1,58 +1,62 @@
 import requests
 
-# Park IDs, as available on Queue-Times.com website.
+# Park IDs, "translating" from words to IDs.
 PARK_IDS = {
-    "Magic Kingdom": 6,
-    "EPCOT": 5,
-    "Disney's Hollywood Studios": 7,
-    "Disney's Animal Kingdom": 8,
-    "Disneyland Park Paris": 4,
-    "Disney Adventure World": 28,
-    "Disneyland Park": 16,
-    "Disney California Adventure": 17,
-    "Hong Kong Disneyland": 31,
-    "Shanghai Disneyland": 30,
-    "Tokyo Disneyland": 274,
-    "Tokyo DisneySea": 275
+    "Magic Kingdom": "75ea578a-adc8-4116-a54d-dccb60765ef9",
+    "EPCOT": "47f90d2c-e191-4239-a466-5892ef59a88b",
+    "Disney's Hollywood Studios": "288747d1-8b4f-4a64-867e-ea7c9b27bad8",
+    "Disney's Animal Kingdom": "1c84a229-8862-4648-9c71-378ddd2c7693",
+    "Disneyland Park Paris": "dae968d5-630d-4719-8b06-3d107e944401",
+    "Disney Adventure World": "ca888437-ebb4-4d50-aed2-d227f7096968",
+    "Tokyo Disneyland": "3cc919f1-d16d-43e0-8c3f-1dd269bd1a42",
+    "Tokyo DisneySea": "67b290d5-3478-4f23-b601-2f8fb71ba803",
+    "Shanghai Disneyland": "ddc4357c-c148-4b36-9888-07894fe75e83",
+    "Disneyland Park": "7340550b-c14d-4def-80bb-acdb51d49a66",
+    "Disney California Adventure": "832fcd51-ea19-4e77-85c7-75d5843b127c",
+    "Hong Kong Disneyland": "bd0eb47b-2f02-4d4d-90fa-cb3a68988e3b"
 }
 
-def getParkAttractionInfo(park: str): # Returns info directly from request with no formatting.
+BASE_URL = "https://api.themeparks.wiki/v1"
+
+
+def getParkAttractionsInfo(park: str):
     if park in PARK_IDS:
-        request = requests.get(f"https://queue-times.com/parks/{PARK_IDS[park]}/queue_times.json")
+        request = requests.get(f"{BASE_URL}/entity/{PARK_IDS[park]}/live")
 
         if request.status_code == 200:
-            return request.json()
+            infoDict = [attr for attr in request.json()["liveData"] if attr["entityType"] == "ATTRACTION"]
+
+            return infoDict
 
     return False
 
 
 def getParkAttractions(park: str): # Return all ride names from a specific park
-    if park.lower() in [pid.lower() for pid in PARK_IDS]:
-        request = requests.get(f"https://queue-times.com/parks/{PARK_IDS[park]}/queue_times.json")
+    info = getParkAttractionsInfo(park)
+    if info:
+        attrs = [attr["name"] for attr in info]
 
-        if request.status_code == 200:
-            rides = []
-
-            for land in request.json()["lands"]:
-                for attraction in land["rides"]:
-                    rides.append(attraction["name"])
-
-            return rides
+        return attrs
 
     return False
+
 
 def getParkWaits(park: str): # Similar to getParkAttractionsInfo, but formats to include only the attraction names and wait times.
-    attractionInfo = getParkAttractionInfo(park)
-    if attractionInfo:
-        ridesDict = {}
+    info = getParkAttractionsInfo(park)
 
-        for land in attractionInfo["lands"]:
-            for attraction in land["rides"]:
-                ridesDict[attraction["name"]] = attraction["wait_time"]
+    if info:
+        attrDict = {}
 
-        return ridesDict
+        for attr in info:
+            qLength = attr.get("queue", {}).get("STANDBY", {}).get("waitTime")
+
+            if qLength:
+                attrDict[attr["name"]] = qLength
+
+        return attrDict
 
     return False
+
 
 def getRideWait(ride: str, park: str): # getParkWaits but searches for a single attraction
     attractions = getParkAttractions(park)
@@ -66,24 +70,33 @@ def getRideWait(ride: str, park: str): # getParkWaits but searches for a single 
 
     return False
 
-def getParkStatuses(park: str): # Similar to getParkAttractionsInfo, but formats to include only the attraction names and whether it is 'Open' or 'Closed'.
-    attractionInfo = getParkAttractionInfo(park)
-    if attractionInfo:
+
+def getParkAttractionStatuses(park: str): # Similar to getParkAttractionsInfo, but formats to include only the attraction names and whether it is 'Open', 'Closed' or 'Down'.
+    info = getParkAttractionsInfo(park)
+    if info:
         ridesDict = {}
 
-        for land in attractionInfo["lands"]:
-            for attraction in land["rides"]:
-                ridesDict[attraction["name"]] = ("Open" if attraction["is_open"] == True else "Closed")
+        for attr in info:
+            if attr["status"] == "OPERATING":
+                ridesDict[attr["name"]] = "Open"
+            elif attr["status"] == "CLOSED":
+                ridesDict[attr["name"]] = "Closed"
+            elif attr["status"] == "DOWN":
+                ridesDict[attr["name"]] = "Down"
+            elif attr["status"] == "REFURBISHMENT":
+                ridesDict[attr["name"]] = "Under refurbishment"
+            else:
+                ridesDict[attr["name"]] = "Unknown"
 
         return ridesDict
 
     return False
 
-def getRideStatus(ride: str, park: str): # getParkStatuses but searches for a single attraction
+def getRideStatus(ride: str, park: str): # getParkAttractionStatuses but searches for a single attraction
     attractions = getParkAttractions(park)
 
     if attractions and (ride.lower() in [attr.lower() for attr in attractions]):
-        parkStatuses = getParkStatuses(park)
+        parkStatuses = getParkAttractionStatuses(park)
 
         if parkStatuses:
             if ride in parkStatuses:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='disneyqueuetimes',
-    version='2026.1',
+    version='2026.2',
     packages=find_packages(),
     install_requires=[
         "requests"
